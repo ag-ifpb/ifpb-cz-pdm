8ef626d6d1c891842e825b91214525311ed349ad
Ari Garcia <aristofanio@hotmail.com>
2017-02-03 14:09:30 -0300

    adicionado botões a tela(frame) de missões

--- FisrtApp/app/src/main/java/pdm/agifpb/firstapp/MainActivity.java
+++ FisrtApp/app/src/main/java/pdm/agifpb/firstapp/MainActivity.java
@@ -1,7 +1,11 @@
 package pdm.agifpb.firstapp;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,30 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //
+        Button tabChat = (Button) findViewById(R.id.tab_chat);
+        Button tabMissions = (Button) findViewById(R.id.tab_missions);
+        //
+        final LinearLayout tabFrameChat = (LinearLayout) findViewById(R.id.tab_frame_chat);
+        final LinearLayout tabFrameMissions = (LinearLayout) findViewById(R.id.tab_frame_missions);
+        //
+        tabChat.setOnClickListener(new Button.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                Log.d("[AGDebug]", "Abrindo o tab do chat.");
+                tabFrameChat.setVisibility(View.VISIBLE);
+                tabFrameMissions.setVisibility(View.GONE);
+            }
+        });
+        //
+        tabMissions.setOnClickListener(new Button.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                Log.d("[AGDebug]", "Abrindo o tab do missiões.");
+                tabFrameChat.setVisibility(View.GONE);
+                tabFrameMissions.setVisibility(View.VISIBLE);
+            }
+        });
+
     }
 }
